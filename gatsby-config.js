596659d521d6b8f0b63ab493ebc8fa20cf98e28c
danielbh <danielhollcraft@gmail.com>
2018-03-04 20:38:21 -0500

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,16 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-40454074-3',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+      },
+    },
     // {
     //   resolve: 'gatsby-plugin-typography',
     //   options: {
